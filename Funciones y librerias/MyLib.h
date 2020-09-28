1f31f77e7bff4054ee4c3661b24bd7bc77142645
GastonSzeman <63881450+GastonSzeman@users.noreply.github.com>
2020-09-28 11:18:29 -0300

    Add files via upload

---
+++ Funciones y librerias/MyLib.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <windows.h>
+#include <time.h>
+
+FILE *p_alarma;
+
+typedef struct {
+	int puertas,capot,baul,velocidad;
+}sensores_t;
+
+sensores_t apagada(void);
+sensores_t alerta(void);
+sensores_t disparada(void);
+sensores_t modificar(int x);
+sensores_t archivo(int x);
+int principal(void);
+int espera(void);
+void historial(int x);
+
